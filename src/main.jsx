5eed59e9d817ae255c841eaefd8f6023a7d90460
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-11-14 12:39:52 +0200

    Fix main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 
 import './index.css';
-import App from './components/App/App';
+import App from './components/App/App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
