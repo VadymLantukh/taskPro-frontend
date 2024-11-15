7f26305bb90dfe2a56e60a617bc21fa2f2b0edad
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-11-15 11:23:49 +0100

    mobile btn

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <div></div>;
+import s from './Button.module.css';
+
+const Button = ({ onClick, text, icon: Icon, className = '' }) => {
+  const buttonClasses = `${s.btn} ${Icon ? s.withIcon : ''} ${className}`;
+
+  return (
+    <button onClick={onClick} className={`${s.btn} ${className}`}>
+      {Icon && <Icon className={s.icon} />}
+      {text && <span className={s.text}>{text}</span>}
+    </button>
+  );
 };
 
 export default Button;
