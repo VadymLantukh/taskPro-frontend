13298d84dcd5d80641412fa6f1c7fff627e4a961
dekizber <dekizber@gmail.com>
2024-11-15 12:50:44 +0200

    init HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,16 @@
 import s from './HomePage.module.css';
+import Header from '../../components/Header/Header';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import ScreensPage from '../../components/ScreensPage/ScreensPage';
 
 const HomePage = () => {
-  return <div className={s.homePage}>HomePage</div>;
+  return (
+    <div className={s.homePage}>
+      <Header />
+      <Sidebar />
+      <ScreensPage />
+    </div>
+  );
 };
 
 export default HomePage;
