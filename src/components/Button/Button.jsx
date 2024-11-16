b4cb8a53e034e450e0da0da504cbc25c4c2ea4a1
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-11-16 22:12:09 +0100

    button Component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,18 @@
 import s from './Button.module.css';
+import svg from '../../images/icons.svg';
 
-const Button = ({ onClick, text, icon: Icon, className = '' }) => {
-  const buttonClasses = `${s.btn} ${Icon ? s.withIcon : ''} ${className}`;
+const Button = ({ onClick, text, showIcon = false, className = '' }) => {
+  const buttonClasses = `${s.btn} ${showIcon ? s.withIcon : ''} ${className}`;
 
   return (
-    <button onClick={onClick} className={`${s.btn} ${className}`}>
-      {Icon && <Icon className={s.icon} />}
+    <button onClick={onClick} className={buttonClasses}>
+      {showIcon && (
+        <span className={s.iconWrapper}>
+          <svg className={s.icon}>
+            <use href={`${svg}#icon-plus`}></use>
+          </svg>
+        </span>
+      )}
       {text && <span className={s.text}>{text}</span>}
     </button>
   );
