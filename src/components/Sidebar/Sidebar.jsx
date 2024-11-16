d7ad31cef44046f0cceea1dc76e074f624b8ada3
Julia <jpkoord@gmail.com>
2024-11-16 07:53:12 +0200

    8 sidebar add createboard/ logout  (#42)
    * LogoComponent
    
    * props
    
    * add CreateBoard / logout

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,40 @@
 import s from './Sidebar.module.css';
 import LogoComponent from './LogoComponent/LogoComponent.jsx';
+import NewBoard from '../NewBoard/NewBoard.jsx';
+import NeedHelp from '../NeedHelp/NeedHelp.jsx';
+import svg from '../../images/icons.svg';
 
 const Sidebar = () => {
   return (
     <div className={s.sidebar}>
-      <LogoComponent />
+      <div>
+        <LogoComponent />
+      </div>
+      <p className={s.sidebarTitle}>My boards</p>
+      <div className={s.createNew}>
+        <p>Create a new board</p>
+        <button className={s.createBoardBtn}>
+          <svg className={s.btnPlus}>
+            <use href={`${svg}#icon-plus`}></use>
+          </svg>
+        </button>
+      </div>
+      <div className={s.project}>
+        <NewBoard />
+        {/* тут у нас дошки треба щоб можна було бачити всi */}
+      </div>
+      <div>
+        <NeedHelp />
+      </div>
+
+      <div className={s.sidedbarLogout}>
+        <button className={s.logoutBtn}>
+          <svg className={s.iconlogout}>
+            <use href={`${svg}#icon-logout`}></use>
+          </svg>
+          <p className={s.textLogout}>Log out</p>
+        </button>
+      </div>
     </div>
   );
 };
