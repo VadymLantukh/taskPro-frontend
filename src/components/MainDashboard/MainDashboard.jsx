07e8ca2ee28b79bedaff9cdd828e5500038c7497
Anastasia <n.sinawka@gmail.com>
2024-11-18 00:43:00 +0200

    (board) added button, headerDashboard

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -1,5 +1,16 @@
-const MainDashboard = () => {
-  return <div></div>;
+import Button from '../Button/Button.jsx';
+
+import s from "./MainDashboard.module.css"
+
+export const MainDashboard = () => {
+  const isEmptyColumn = true;
+  return (
+    <div>
+      {isEmptyColumn &&
+      <Button showIcon={true} text="Add another column" className={s.button}/>
+      }
+    </div>
+  );
 };
 
 export default MainDashboard;
--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,11 +1,15 @@
 import { useParams } from 'react-router-dom';
+import HeaderDashboard from '../../components/HeaderDashboard/HeaderDashboard.jsx';
+import MainDashboard from '../../components/MainDashboard/MainDashboard.jsx';
+
+import styles from './ScreensPage.module.css';
 
 const ScreensPage = () => {
   const { boardId } = useParams();
   return (
-    <div>
-      ScreensPage
-      {boardId}
+    <div className={styles.container}>
+      <HeaderDashboard title={boardId}/>
+      <MainDashboard/>
     </div>
   );
 };
