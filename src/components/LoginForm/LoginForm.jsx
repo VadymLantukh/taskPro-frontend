9db92d85e66c84ad0aadf17ef4cadcf52fcd3530
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-11-17 15:50:04 +0200

    5 componentsregisterform (#48)
    * add basic login and register forms
    
    * added register and login forms, validationSchema in helpers, setting Auth Page
    
    * update
    
    * update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,64 @@
+import { useDispatch } from 'react-redux';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { logIn } from '../../redux/auth/authOperations.js';
+import { logInSchema } from '../../helpers/logInSchema.js';
+
+import { MdOutlineRemoveRedEye } from 'react-icons/md';
+
+import s from './LoginForm.module.css';
+
 const LoginForm = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  const initialValues = { email: '', password: '' };
+
+  const handleSubmit = (values, action) => {
+    dispatch(logIn(values));
+    action.resetForm();
+  };
+  return (
+    <div className={s.wrapper}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={logInSchema}
+        validateOnChange={false}
+      >
+        <Form className={s.form}>
+          <label>
+            <Field
+              type="email"
+              name="email"
+              className={s.input}
+              placeholder="Enter your email"
+            />
+            <ErrorMessage
+              name="email"
+              component="span"
+              className={s.errorForEmail}
+            />
+          </label>
+          <label>
+            <Field
+              type="password"
+              name="password"
+              className={s.input}
+              placeholder="Confirm a password"
+            />
+            <MdOutlineRemoveRedEye />
+            <ErrorMessage
+              name="password"
+              component="span"
+              className={s.errorForPassword}
+            />
+          </label>
+          <button type="submit" className={s.btn}>
+            Log In Now
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default LoginForm;
--- src/utils/devHelper.jsx
+++ src/utils/devHelper.jsx
@@ -1,1 +1,1 @@
-export const isLoggedIn = true;
+export const isLoggedIn = false;
