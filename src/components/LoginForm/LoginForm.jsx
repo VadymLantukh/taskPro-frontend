20f29269d62d212b70f7eeb356dabbef44b8457e
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-11-17 22:38:56 +0200

    finished Register, Login Components/ Update Welcome Component

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,16 +1,25 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
+
 import { logIn } from '../../redux/auth/authOperations.js';
 import { logInSchema } from '../../helpers/logInSchema.js';
 
-import { MdOutlineRemoveRedEye } from 'react-icons/md';
+import { MdOutlineRemoveRedEye, MdOutlineVisibilityOff } from 'react-icons/md';
 
 import s from './LoginForm.module.css';
 
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const dispatch = useDispatch();
 
   const initialValues = { email: '', password: '' };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = (values, action) => {
     dispatch(logIn(values));
@@ -22,37 +31,59 @@ const LoginForm = () => {
         initialValues={initialValues}
         onSubmit={handleSubmit}
         validationSchema={logInSchema}
-        validateOnChange={false}
+        validateOnBlur={true}
       >
         <Form className={s.form}>
+          <div className={s.linkNav}>
+            <NavLink to="/auth/register" className={s.registerLink}>
+              Registration
+            </NavLink>
+            <NavLink to="/auth/login" className={s.loginLink}>
+              Log In
+            </NavLink>
+          </div>
           <label>
-            <Field
-              type="email"
-              name="email"
-              className={s.input}
-              placeholder="Enter your email"
-            />
+            <div>
+              <Field
+                type="email"
+                name="email"
+                className={s.input}
+                placeholder="Enter your email"
+              />
+            </div>
             <ErrorMessage
               name="email"
               component="span"
-              className={s.errorForEmail}
+              className={s.errorMessage}
             />
           </label>
           <label>
-            <Field
-              type="password"
-              name="password"
-              className={s.input}
-              placeholder="Confirm a password"
-            />
-            <MdOutlineRemoveRedEye />
+            <div className={s.passwordWrapper}>
+              <Field
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                className={s.input}
+                placeholder="Confirm a password"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className={s.eyeButton}
+              >
+                {showPassword ? (
+                  <MdOutlineVisibilityOff size="18" />
+                ) : (
+                  <MdOutlineRemoveRedEye size="18" />
+                )}
+              </button>
+            </div>
             <ErrorMessage
               name="password"
               component="span"
-              className={s.errorForPassword}
+              className={s.errorMessage}
             />
           </label>
-          <button type="submit" className={s.btn}>
+          <button type="submit" className={s.btnSubmit}>
             Log In Now
           </button>
         </Form>
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,12 +1,19 @@
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 
 import { register } from '../../redux/auth/authOperations';
 import { registrationSchema } from '../../helpers/registrationSchema';
 
+import { MdOutlineRemoveRedEye, MdOutlineVisibilityOff } from 'react-icons/md';
+
 import s from './RegisterForm.module.css';
 
 const RegisterForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const dispatch = useDispatch();
 
   const initialValues = {
@@ -15,9 +22,11 @@ const RegisterForm = () => {
     password: '',
   };
 
-  const handleSubmit = (values, action) => {
-    console.log(values);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
+  const handleSubmit = (values, action) => {
     dispatch(register(values));
     action.resetForm();
   };
@@ -28,51 +37,74 @@ const RegisterForm = () => {
         initialValues={initialValues}
         onSubmit={handleSubmit}
         validationSchema={registrationSchema}
-        // validateOnBlur={true}
-        validateOnChange={false}
+        validateOnBlur={true}
       >
         <Form className={s.form}>
+          <div className={s.linkNav}>
+            <NavLink to="/auth/register" className={s.registerLink}>
+              Registration
+            </NavLink>
+            <NavLink to="/auth/login" className={s.loginLink}>
+              Log In
+            </NavLink>
+          </div>
           <label>
-            <Field
-              type="text"
-              name="name"
-              className={s.input}
-              placeholder="Enter your name"
-            />
+            <div>
+              <Field
+                type="text"
+                name="name"
+                className={s.input}
+                placeholder="Enter your name"
+              />
+            </div>
             <ErrorMessage
               name="name"
               component="span"
-              className={s.errorForName}
+              className={s.errorMessage}
             />
           </label>
           <label>
-            <Field
-              type="email"
-              name="email"
-              className={s.input}
-              placeholder="Enter your email"
-            />
+            <div>
+              <Field
+                type="email"
+                name="email"
+                className={s.input}
+                placeholder="Enter your email"
+              />
+            </div>
             <ErrorMessage
               name="email"
               component="span"
-              className={s.errorForEmail}
+              className={s.errorMessage}
             />
           </label>
           <label>
-            <Field
-              type="password"
-              name="password"
-              className={s.input}
-              placeholder="Create a password"
-            />
+            <div className={s.passwordWrapper}>
+              <Field
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                className={s.input}
+                placeholder="Create a password"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className={s.eyeButton}
+              >
+                {showPassword ? (
+                  <MdOutlineVisibilityOff size="18" />
+                ) : (
+                  <MdOutlineRemoveRedEye size="18" />
+                )}
+              </button>
+            </div>
             <ErrorMessage
               name="password"
               component="span"
-              className={s.errorForPassword}
+              className={s.errorMessage}
             />
           </label>
-
-          <button type="submit" className={s.btn}>
+          <button type="submit" className={s.btnSubmit}>
             Register Now
           </button>
         </Form>
