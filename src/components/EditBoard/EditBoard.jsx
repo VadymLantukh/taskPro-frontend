0a7be84b195f44de61f3d54483f8fdf63b0bcbb4
gramatchi <nik98.champion@gmail.com>
2024-11-18 18:19:55 +0100

    11 componentsnewboard (#59)
    * mobile-white v1
    
    * add universal button
    
    * add validation on form
    
    * delete extra modal fix container background
    
    * add universal form for board
    
    * add plug for edit form
    
    * delete comments
    
    * delete comments

--- src/components/EditBoard/EditBoard.jsx
+++ src/components/EditBoard/EditBoard.jsx
@@ -1,5 +1,28 @@
-const EditBoard = () => {
-  return <div></div>;
+import BoardForm from '../BoardForm/BoardForm';
+
+const EditBoard = ({ boardToEdit = {} }) => {
+  const defaultBoard = {
+    title: 'Sample Board',
+    icon: 'icon_2',
+    background: 'bg_0',
+  };
+
+  const { title, icon, background } = { ...defaultBoard, ...boardToEdit };
+
+  const handleUpdateBoard = updatedBoardData => {
+    console.log('Board Updated:', updatedBoardData);
+  };
+
+  return (
+    <BoardForm
+      initialTitle={title}
+      initialSelectedIcon={icon}
+      initialSelectedBackground={background}
+      formTitle="Edit board"
+      buttonText="Edit"
+      onSubmit={handleUpdateBoard}
+    />
+  );
 };
 
 export default EditBoard;
