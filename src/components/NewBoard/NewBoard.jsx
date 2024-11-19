b427365be04904726ce65f8272c84ebacb2361c4
gramatchi <nik98.champion@gmail.com>
2024-11-19 22:12:51 +0100

    11 componentsnewboard (#87)
    * mobile-white v1
    
    * add universal button
    
    * add validation on form
    
    * delete extra modal fix container background
    
    * add universal form for board
    
    * add plug for edit form
    
    * delete comments
    
    * delete comments
    
    * add formik fix images background
    
    * fix path images
    
    * add add board onClick

--- src/components/NewBoard/NewBoard.jsx
+++ src/components/NewBoard/NewBoard.jsx
@@ -1,16 +1,40 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addBoard } from '../../redux/board/boardOperations';  
+import { selectIsLoading, selectIsError } from '../../redux/board/boardSelectors'; 
 import BoardForm from '../BoardForm/BoardForm';
 
 const NewBoard = () => {
-  const handleCreateBoard = boardData => {
-    console.log('New Board Created:', boardData);
+  const dispatch = useDispatch();
+
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectIsError);
+
+  const handleCreateBoard = async boardData => {
+    const requestData = {
+      title: boardData.title,
+      icon: boardData.selectedIcon,
+      backgroundImage: boardData.selectedBackground,
+    };
+  
+    try {
+      await dispatch(addBoard(requestData)).unwrap();
+      console.log('New Board Created:', requestData);
+    } catch (err) {
+      console.error('Failed to create board:', err.response?.data || err.message);
+    }
   };
+  
 
   return (
-    <BoardForm
-      formTitle="New board"
-      buttonText="Create"
-      onSubmit={handleCreateBoard}
-    />
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <BoardForm
+        formTitle="New board"
+        buttonText="Create"
+        onSubmit={handleCreateBoard}
+      />
+    </div>
   );
 };
 
