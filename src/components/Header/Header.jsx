55c92d2b5152191c191427dd19f3dd9d69adbcbd
lukinivan <Lukinivan09@gmail.com>
2024-11-19 10:08:46 +0000

    add header elements (#68)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,28 @@
+import Icon from '../Icon/Icon';
 import s from './Header.module.css';
 
 const Header = () => {
-  return <header className={s.header}>Header</header>;
+  return (
+    <header className={s.header}>
+      <div>
+        <Icon name={'icon-menu'} className={s.menu_icon} />
+      </div>
+      <div className={s.theme_user_wrap}>
+        <div className={s.theme}>
+          <span>Theme</span>
+          <Icon name={'icon-arrowDown'} className={s.arrow_icon} />
+        </div>
+        <div className={s.profile}>
+          <span>name</span>
+          <img
+            src="https://via.placeholder.com/40"
+            alt="Profile"
+            className={s.profile_image}
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
