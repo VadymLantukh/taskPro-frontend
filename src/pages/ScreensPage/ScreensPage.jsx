983cc80b21251c33fdbca60ecf4066d546a0a334
koleso8 <dev.koleso@gmail.com>
2024-11-19 11:22:48 +0200

    fix redux

--- src/pages/ScreensPage/ScreensPage.jsx
+++ src/pages/ScreensPage/ScreensPage.jsx
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getUserThunk } from '../../redux/auth/authOperations';
+import { useEffect } from 'react';
+import { selectUser } from '../../redux/auth/authSelectors';
 
 const ScreensPage = () => {
   const { boardId } = useParams();
+  // const { id } = useSelector(selectUser);
+  console.log('getter');
+
+  useEffect(() => {
+    // const board = dispatch(getBoard(boardId))
+  });
+
   return (
     <div>
       ScreensPage
