3bb7e908048627dcede0b0ef7d3d18d71d859a7d
koleso8 <dev.koleso@gmail.com>
2024-11-20 11:36:04 +0200

    fix dedux

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,25 @@
 import { Suspense, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
 import Loader from '../Loader/Loader';
 
-import s from './Layout.module.css';
-import { useDispatch } from 'react-redux';
 import { getUserThunk } from '../../redux/auth/authOperations';
+import { setTheme } from '../../redux/auth/authSlice';
+import { selectTheme } from '../../redux/auth/authSelectors';
+
+import s from './Layout.module.css';
 
 export const Layout = () => {
   const dispatch = useDispatch();
+  const theme = useSelector(selectTheme);
 
   useEffect(() => {
     dispatch(getUserThunk());
-  }, []);
+    dispatch(setTheme());
+  }, [dispatch, theme]);
   return (
     <div className={s.page}>
       <div className={s.wrapper}>
