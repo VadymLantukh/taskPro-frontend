b816eaa7c2fb560640ba37c053c0c23d28c033ad
Dima Hutsol <dimaaa291997@gmail.com>
2024-11-20 17:11:13 +0000

    32 componentsaddcard (#102)
    * update, not completed
    
    * Update HomePage.jsx
    
    * Update HomePage.jsx
    
    * add components wo redux
    
    * addCard, EditCard logic, not completed

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,17 +1,33 @@
+import { useState } from 'react';
+
 import HeaderColumn from './HeaderColumn/HeaderColumn.jsx';
 import Button from '../Button/Button.jsx';
 import TasksList from '../TasksList/TasksList.jsx';
+import ModalWrapper from '../../components/ModalWrapper/ModalWrapper';
+import AddCard from '../../components/AddCard/AddCard';
 
-import s from "./Column.module.css"
+import s from './Column.module.css';
 
-export const Column = ({column}) => {
-  const title = column?.title ?? ""
+export const Column = ({ column }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
+  const title = column?.title ?? '';
 
   return (
     <div className={s.container}>
-      <HeaderColumn title={title}/>
-      <TasksList columnId={column?._id }/>
-      <Button text="Add another card" showIcon={true} />
+      <HeaderColumn title={title} />
+      <TasksList columnId={column?._id} />
+      <Button
+        text="Add another card"
+        showIcon={true}
+        onClick={handleOpenModal}
+      />
+      <ModalWrapper open={isModalOpen} onClose={handleCloseModal}>
+        <AddCard />
+      </ModalWrapper>
     </div>
   );
 };
