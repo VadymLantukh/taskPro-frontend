1a0e8b018b22076c3870da38fff6217059a08d00
dekizber <dekizber@gmail.com>
2024-11-21 23:11:17 +0200

    stylish text homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,14 +2,16 @@ import s from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div className={s.homePage}>
-      Before starting your project, it is essential&nbsp;
-      <button className={s.btn} onClick={() => console.log('click')}>
-        to create a board&nbsp;
-      </button>
-      to visualize and track all the necessary tasks and milestones. This board
-      serves as a powerful tool to organize the workflow and ensure effective
-      collaboration among team members.
+    <div className={s.homePage_container}>
+      <p className={s.text}>
+        Before starting your project, it is essential&nbsp;
+        <button className={s.btn} onClick={() => console.log('click')}>
+          to create a board&nbsp;
+        </button>
+        to visualize and track all the necessary tasks and milestones. This
+        board serves as a powerful tool to organize the workflow and ensure
+        effective collaboration among team members.
+      </p>
     </div>
   );
 };
