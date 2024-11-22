dcfc63bdc69548e370f6357f77a256ac2868f2fe
gramatchi <nik98.champion@gmail.com>
2024-11-22 09:17:08 +0100

    12 componentseditboard (#119)
    * fix default background,  delete paragraph, add onClose on form addBoard
    
    * add editBoars, change BoardForm, BoardsItem
    
    ---------
    
    Co-authored-by: Vadym_Lantukh <vadym.lantukh.1998@gmail.com>

--- src/components/BoardsItem/BoardsItem.jsx
+++ src/components/BoardsItem/BoardsItem.jsx
@@ -3,15 +3,26 @@ import s from './BoardsItem.module.css';
 import Icon from '../Icon/Icon';
 import { deleteBoard, updateBoard } from '../../redux/board/boardOperations';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import Modal from '../ModalWrapper/ModalWrapper';
+import EditBoard from '../EditBoard/EditBoard';
+
 const BoardsItem = ({ title, id, icon, backgroundImage }) => {
-  const board = { title, id, icon, backgroundImage };
+  const board = { id, title, icon, background: backgroundImage };
   const dispatch = useDispatch();
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false); 
+
+
+  const handleEditOpen = () => setIsEditModalOpen(true);
+  const handleEditClose = () => setIsEditModalOpen(false);
+
   return (
+    <>
     <li>
       <NavLink to={id} className={s.list_item}>
         <Icon name={icon} fill="none" width={18} height={18} />
         <p className={s.title}>{title}</p>
-        <button onClick={() => dispatch(updateBoard(board))}>
+        <button onClick={handleEditOpen}>
           <Icon name={'icon-pencil'} fill="none" width={16} height={16} />
         </button>
         <button onClick={() => dispatch(deleteBoard(id))}>
@@ -19,6 +30,15 @@ const BoardsItem = ({ title, id, icon, backgroundImage }) => {
         </button>
       </NavLink>
     </li>
+    {isEditModalOpen && (
+      <Modal open={isEditModalOpen} onClose={handleEditClose}>
+        <EditBoard
+          boardToEdit={board}
+          onClose={handleEditClose}
+        />
+      </Modal>
+    )}
+    </>
   );
 };
 
--- src/redux/board/boardOperations.js
+++ src/redux/board/boardOperations.js
@@ -53,7 +53,9 @@ export const updateBoard = createAsyncThunk(
   'boards/updateBoard',
   async ({ board, id }, thunkAPI) => {
     try {
-      const { data } = await axios.patch(`/boards/${id}`, { board });
+      const { id, ...updateFields } = board; 
+
+      const { data } = await axios.patch(`/boards/${id}`, updateFields); 
       return data.data.data;
     } catch (error) {
       //   toast.error(
